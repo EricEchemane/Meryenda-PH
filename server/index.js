032f7b86201ff651e475eaa08c57b77b930327f5
Eric Echemane <eechemane29@gmail.com>
2022-01-12 20:16:27 +0800

    Configured index.js to use dotenv and make the PORT dynamic base on the node environment

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 2900;
+const PORT = process.env.PORT || 2900;
 const cors = require('cors');
 
-require('dotenv').config();
+const { authenticateUser } = require('./lib/authentication');
+const login = require('./routes_handlers/login');
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', authenticateUser, (req, res) => {
     res.send({
         message: 'Welcome to Meryenda Server',
         author: 'Eric Echemane',
         version: "1.0.0"
     });
 });
+
+app.post('/login', login);
 
 app.listen(PORT, () => console.log(`Meryenda Server running on port ${PORT}`));
