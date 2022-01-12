9e1372b5f0865d75b2162ea9140c21b4e3330cf0
Eric Echemane <eechemane29@gmail.com>
2022-01-12 20:37:27 +0800

    Removed /server file

--- server/lib/connection.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require('mongodb');
-export const URI = process.env.DEV_URI;
-
-export const Client = new MongoClient(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
